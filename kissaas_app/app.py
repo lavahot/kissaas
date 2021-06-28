7c6a9193d62aeaee57b0376225f584610491b576
LavaHot <lavahot@gmail.com>
2021-06-28 22:57:58 +0000

    Added basic app functionality

---
+++ kissaas_app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from random import choice
+
+app = Flask(__name__)
+
+inspirations = [
+    "Hang in there, baby.",
+    "If at first you don't succeed, try again.",
+    "You can do it. I know you can.",
+    "Make your biggest dreams come true by working on its smallest parts.",
+    "Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear.",
+    "You have more fans than you think.",
+    "Do Better"
+]
+
+
+@app.route('/')
+@app.route('/kindness')
+def kindness():
+    return choice(inspirations)
